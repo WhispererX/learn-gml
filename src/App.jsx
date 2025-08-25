748c6d40596cc7d3e4de9226cabb7518048ce7c6
Whisperer <whisperer.meta@gmail.com>
2025-08-25 13:34:06 +0300

    Add routing and Coming Soon page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
+import { Link } from 'react-router';
+import { NavLink } from 'react-router';
 
 function App() {
   return (
     <>
-      <h1>Coming Soon</h1>
-      <p>We are working on something awesome!</p>
+      <h1>App</h1>
     </>
   );
 }
---
+++ src/pages/coming-soon/ComingSoon.jsx
@@ -0,0 +1,12 @@
+import './ComingSoon.module.css';
+
+function ComingSoon() {
+  return (
+    <>
+      <h1>Coming Soon</h1>
+      <p>We are working on something awesome!</p>
+    </>
+  );
+}
+
+export default ComingSoon;
