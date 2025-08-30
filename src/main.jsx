026b8d99ea946e47b7a3fd3d8466461fbaeba253
Whisperer <whisperer.meta@gmail.com>
2025-08-30 11:20:12 +0300

    Improve footer responsiveness
    Add light-mode support for the footer
    Fix header z-index

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
-import './styles/index.css'
-import App from './App.jsx'
+import './styles/index.css';
+import App from './App.jsx';
 import ComingSoon from './pages/coming-soon/ComingSoon.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ComingSoon />} />
+        <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
