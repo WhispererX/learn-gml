3b228db364fedf3040345a137f8574cc6b36c611
Whisperer <whisperer.meta@gmail.com>
2025-08-30 12:47:22 +0300

    Move ComingSoon to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
 import Footer from './components/footer/footer';
 import Header from './components/header/header';
+import ComingSoon from './pages/coming-soon/ComingSoon';
 
 function App() {
   return (
     <>
-      <Header />
+      <ComingSoon/>
       <Footer></Footer>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import './styles/index.css';
 import App from './App.jsx';
-import ComingSoon from './pages/coming-soon/ComingSoon.jsx';
 import TermsConditions from './pages/terms/TermsConditions.jsx';
 import PrivacyPolicy from './pages/privacy/PrivacyPolicy.jsx';
 
